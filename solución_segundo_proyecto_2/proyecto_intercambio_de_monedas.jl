aa4da41b6647226bae30d6594d8e2351d608a91f
Ever <ee.quintero@uniandes.edu.co>
2021-01-27 20:57:33 -0500

    Actualización del repositorio

--- solución_segundo_proyecto_2/proyecto_intercambio_de_monedas.jl
+++ solución_segundo_proyecto_2/proyecto_intercambio_de_monedas.jl
@@ -1,16 +1,17 @@
 using  Pkg; Pkg.activate(".")
-using Genie, Genie.Router
-using Genie.Renderer, Genie.Renderer.Json
-import Pkg; Pkg.add("DataFrames"); Pkg.add("Queryverse")
-using Queryverse, DataFrames ##Importando librerías
-using Genie.Renderer.Html, Genie.Requests
-rate= load("FRB_H10_rows (2).csv")
+import Pkg; Pkg.add("DataFrames"); Pkg.add("Queryverse"); Pkg.add("Genie"); Pkg.add("JSONTables")
+using Queryverse, DataFrames, JSONTables ##Importando librerías
+using Genie, Genie.Router, Genie.Renderer, Genie.Renderer.Json, Genie.Renderer.Html, Genie.Requests
 
-dfrate= DataFrame(rate)
+begin #Preparación del DataFrame
+    rate= load("FRB_H10_rows.csv")
+    dfrate= DataFrame(rate)[Not([:"Multiplier:", :"Descriptions:", :"Unique Identifier:", :"Series Name:"])] #Quitamos la columnas que no importan
+    dfrate= dfrate[describe(dfrate).min.!="ND"] #Quitamos las columnas sin datos (No Data)
 
-rename!(dfrate, :"Currency:"=>"Currency")
-rename!(dfrate, :"Unit:"=>"Unit")
-names(dfrate)
+    rename!(dfrate, :"Currency:"=>"Currency")
+    rename!(dfrate, :"Unit:"=>"Unit")
+    names(dfrate)
+end
 
 #Función que da la tasa de cambio, más reciente, entre dos tipos de moneda
 function divisa(moneda_base::String, moneda_particular::String)
@@ -61,7 +62,6 @@ route("/POST/last", method = POST) do
     return "La tasa de intercambio actual entre $(moneda_1) y $(moneda_2) es $(tasa)"
 end
 
-
 form2 = """
 <form action="/POST/converter" method="POST" enctype="multipart/form-data">
   <input type="text" name="moneda1" value="" placeholder="Ingrese Moneda base" />
@@ -102,7 +102,7 @@ route("/POST/date", method = POST) do
     moneda_2=postpayload(:moneda2,"JPY")
     fecha=postpayload(:fecha,"1/1/2021")
     tasa=divisa(moneda_1,moneda_2,fecha)
-    return "La tasa de intercambio entre $(moneda_1) y $(moneda_2) el dia $(fecha) es $(tasa)"
+    return arraytable(tasa)
 end
 
-up(8000)
+up(8005)
